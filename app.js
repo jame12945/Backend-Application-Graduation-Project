2315493c7c21e8fa6c594f0f1bb10db5c4e09da6
jame12945 <jame12945@gmail.com>
2023-10-23 13:50:21 +0700

    can login

--- app.js
+++ app.js
@@ -1,172 +1,93 @@
 var express = require('express')
 var cors = require('cors')
+var app = express()
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
-var app = express()
-//hash
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-//token
 var jwt = require('jsonwebtoken');
-const secret = 'login-2023'
-// var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+const secret = 'Fullstack-Login-2021'
 
 
 app.use(cors())
 
 const mysql = require('mysql2');
+// const req = require('express/lib/request');
+// const { timeout } = require('nodemon/lib/config')
 const connection = mysql.createConnection({
+    // password: 'Kongilllk5555',
     host: 'localhost',
     user: 'root',
     database: 'projectfinaldb'
-  });
+});
 
-app.post('/register', jsonParser,function (req, res, next) {
+
+
+
+app.post('/register', jsonParser, function (req, res, next) {
+  bcrypt.hash(req.body.user_password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
     const attendeeid = 1
-    connection.execute(//INSERT INTO + table in db name
-        'INSERT INTO user_insystem(user_username,user_password,user_Fname,user_Lname,user_phone,user_email,user_ishost,user_faceimagefile,attendee_id) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [   
-            req.body.user_username,
-            req.body.user_password,
-            req.body.user_Fname,
-            req.body.user_Lname,
-            req.body.user_phone,
-            req.body.user_email,
-            req.body.user_ishost,
-            req.body.user_faceimagefile,
-            attendeeid
-        ],
-        function(err, results, fields) {
-          if(err){
-            res.json({status : "error", message:err})
-            return
-          }
-          res.json({status : 'ok'})
-        }
-      );
-
-  
-  
+    connection.execute(
+      'INSERT INTO user_insystem(user_username,user_password,user_fname,user_lname,user_phone,user_email,user_ishost,user_faceimagefile,attendee_id) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      [    req.body.user_username,
+           hash, 
+           req.body.user_fname,
+           req.body.user_lname,
+           req.body.user_phone,
+           req.body.user_email,
+           req.body.user_ishost,
+           req.body.user_faceimagefile,
+           attendeeid],
+      function (err, results, fields) {
+       if (err) {
+               res.json({status : 'error', message: err})
+               return
+       }
+     
+       res.json({status: 'ok'})
+      }
+);
+});
+       
+     
 })
-//use token here
+
+
 app.post('/login', jsonParser, function (req, res, next) {
-  if (req.body.user_email === undefined || req.body.user_password === undefined) {
-    res.json({ status: "error", message: "Missing user_email or user_password" });
-    return;
-  }
-
-  console.log('Received request with user_email:', req.body.user_email);
-
   connection.execute(
-    'SELECT * FROM user_insystem WHERE user_email=? AND user_password=?', // เปรียบเทียบรหัสผ่านแบบไม่เข้ารหัส
-    [req.body.user_email, req.body.user_password],
-    function(err, user_insystem, fields) {
-      if (err) {
-        console.log('Error while querying the database:', err);
-        res.json({ status: "error", message: err });
-        return;
+    'SELECT * FROM user_insystem WHERE user_email =?',
+    [req.body.user_email],
+    function (err, user_insystem, fields) {
+     if (err) {
+             res.json({status : 'error', message: err});
+             return
+     }
+     if(user_insystem.length==0){
+      res.json({status : 'error', message: 'no user found'});
+      return
+     }
+     bcrypt.compare(req.body.user_password,user_insystem[0].user_password, function(err, isLogin) {
+      // result == true
+      if(isLogin){
+        res.json({status : 'ok', message: 'login success'})
       }
-      console.log('user_insystem:', user_insystem);
-
-      if (user_insystem.length === 0) {
-        console.log('No users found in the database or passwords do not match.');
-        res.json({ status: "error", message: 'No users found or passwords do not match' });
-        return;
+      else{
+        res.json({status : 'error', message: 'login failed'});
       }
+  });
+   
 
-      console.log('Login successful.');
-      res.json({ status: 'ok', message: 'Login success' });
     }
-  );
-});
+);
+})
 
 
 
-app.post('/reservations', jsonParser, function (req, res, next) {
-  // ดึง "User_Id" จากการเข้าสู่ระบบ (คำสั่งหรือโทเคนการเข้าสู่ระบบ)
-  const userId =10; // ตั้งชื่อตามตัวแปรที่คุณใช้
-  // ต้องทำ token ก่อนตรงนี้ถึงใช้ได้ค่าเลข 6 คือId ที่อยู่ในตาราง User (mock ไว้)
-  // const userId=req.user.Id;   
-  // ไม่ต้องระบุ "User_Id" ในข้อมูลที่ส่งมาใน Postman หรือคำสั่ง SQL INSERT
-  connection.query(
-    'INSERT INTO reservation(RoomId, Date, StartTime, EndTime, RoomNum,  User_Id) VALUES (?, ?, ?, ?, ?, ?)',
-    [
-      req.body.RoomId,
-      req.body.Date,
-      req.body.StartTime,
-      req.body.EndTime,
-      req.body.RoomNum,
-      userId // ใช้ "User_Id" ที่ได้จากการเข้าสู่ระบบ
-    ],
-    function (err, results, fields) {
-      if (err) {
-        res.json({ status: "error", message: err });
-      } else {
-        res.json({ status: 'ok' });
-      }
-    }
-  );
-});
 
 
-app.get('/testget', (req, res) => {
-   res.send('Hello, World!!!!');
- });
+
 
 app.listen(3333, function () {
-  console.log('localhost:3333 + /pathname')
+    console.log('CORS-enabled web server listening on port 3333')
 })
-
-
-
-// ..........................................
-// const express =require('express');
-// const {Server} =require('ws')
-// const PORT = process.env.PORT || 3000
-// const server = express().use((req,res) => res.send('Hello jame two')).listen(PORT, () => console.log(`Listening on ${PORT}`));
-// const wss = new Server({server});
-// wss.on('connection',ws=>{
-//    console.log('Client connected');
-//    ws.on('message', message => console.log(`Recieved: ${message}`));
-//    ws.on('close', () => console.log('Client Disconnected'));
-
-// })
-//................................................
-
-
-/////start
-// const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({
-//       extended:true
-// }))
-// const productData = []
-// app.listen(2000, function () {
-//   console.log('Connected to Server at 2000');
-// })
-
-// app.post("/api/add_product",  (req, res) =>{
-
-//    console.log("Result",req.body);
-//    const pdata = {
-//        "id": productData.length+1,
-//        "pname": req.body.pname,
-//        "pphone": req.body.pphone,
-//        "pemail":req.body.pemail
-
-//    };
-
-//    productData.push(pdata);
-//    console.log("Final",pdata);
-
-//    req.status(200).send({
-//     "status_code": 200,
-//     "message":"Product added successfully",
-//     "product": pdata
-//    })
-
-
-// })
-
-// //End test
