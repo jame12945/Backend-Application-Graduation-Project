3a059993bbd61ac713bbe928fc9bade41775920f
jame12945 <jame12945@gmail.com>
2023-10-23 18:57:22 +0700

    reservation and track user_id

--- app.js
+++ app.js
@@ -144,35 +144,35 @@ app.post('/register', jsonParser, function (req, res, next) {
 // Register----------------------------------------------------------------------------------------------------------------
 
 // Login-------------------------------------------------------------------------------------------------------------------
+
 app.post('/login', jsonParser, function (req, res, next) {
   connection.execute(
-      'SELECT * FROM user_insystem WHERE user_username=?',
-      [req.body.user_username],
-      function (err, users, fields) {
-          if (err) {
-              res.json({ status: 'error', message: err })
-              return
+    'SELECT * FROM user_insystem WHERE user_username=?',
+    [req.body.user_username],
+    function (err, users, fields) {
+      if (err) {
+        res.json({ status: 'error', message: err });
+        return;
+      }
+      if (users.length == 0) {
+        res.json({ status: 'error', message: 'no user found' });
+        return;
+      } else {
+        const userId = users[0].user_id; // ดึง user_id จากผลลัพธ์
+        bcrypt.compare(req.body.user_password, users[0].user_password, function (err, isLogin) {
+          if (isLogin) {
+            const token = jwt.sign({ user_id: userId, username: users[0].user_username }, secret, { expiresIn: '12h' });
+            console.log('user is selected', users[0]);
+            res.json({ status: 'ok', message: 'login success', token });
+          } else {
+            res.json({ status: 'error', message: isLogin });
           }
-          if (users.length == 0) {
-              res.json({ status: 'error', message: 'no user found' })
-              return
-          }else{
-          bcrypt.compare(req.body.user_password, users[0].user_password, function (err, isLogin) {
-              if (isLogin) {
-                  var token = jwt.sign({ username: users[0].user_username }, secret, { expiresIn: '12h' });
-                  console.log('user is selected',users[0])
-                  res.json({ status: 'ok', message: 'login success', token })
+        });
+      }
+    }
+  );
+});
 
-              }
-              
-              else {
-                  res.json({ status: 'error', message: isLogin })
-              }
-          });
-      }
-      }
-  )
-})
 // Login-------------------------------------------------------------------------------------------------------------------
 
 // Authen-------------------------------------------------------------------------------------------------------------------
@@ -187,9 +187,123 @@ app.post('/authen', jsonParser, function (req, res, next) {
   }
 })
 // Authen-------------------------------------------------------------------------------------------------------------------
+// Profile-------------------------------------------------------------------------------------------------------------------
+app.get('/profile/:token', function (req, res) {
+  try {
+      const decode = jwt.verify(req.params.token, secret);
+      const { username } = decode
+      connection.execute(
+          'SELECT * FROM user_insystem WHERE user_username=?',
+          [username],
+          function (err, results) {
+
+              if (err) {
+                  res.json({ status: 'error', message: err })
+                  return
+              }
+              if (results.length == 0) {
+                  res.json({ status: 'error', message: 'no user found' })
+                  return
+              }
+              let Usname = results[0].user_fname + ["   "] + results[0].user_lname
+              let Usemail = results[0].user_email
+              let Usphone = results[0].user_phone
+              res.json({ status: 'ok', message: 'success', usname: Usname, usemail: Usemail, usphone: Usphone })
+              return
+          }
+
+      )
+
+  } catch (err) {
+      res.json({ status: 'error', message: err.message })
+  }
+})
+// Profile-------------------------------------------------------------------------------------------------------------------
+
+// NewPassword-------------------------------------------------------------------------------------------------------------------
+app.put('/newPassword', jsonParser, function (req, res) {
+  const decode = jwt.verify(req.body.token, secret);
+  const { username } = decode
+  connection.query(
+      'SELECT user_password FROM user_insystem WHERE user_username=?', [username],
+      function (err, users, fields) {
+          bcrypt.compare(req.body.user_password, users[0].user_password, function (err, isLogin) {
+
+              if (isLogin) {
+
+                  bcrypt.hash(req.body.user_newpassword, saltRounds, function (err, hash) {
+                        
+                      connection.execute(
+                          'UPDATE user_insystem SET user_password =? WHERE user_username=? ', [hash, username],
+
+                          function (err, results) {
+
+                              if (err) {
+                                  res.json({ status: 'error', message: err })
+                                  return
+                              }
+                              if (results.length == 0) {
+                                  res.json({ status: 'not found user ' })
+                                  return
+                              }
+                              if (req.body.password == hash) {
+                                  res.json({ status: 'error', message: err })
+                                  return
+                              }
+                              else {
+
+                                  res.send(results)
+                                  return
+                              }
+                          }
+                      );
+
+                  });
+              }
+              else {
+                  res.json({ status: 'error', message: 'Your password is incorrect.' })
+              }
+              if (err) {
+                  res.json({ status: 'error', message: err })
+                  return
+              }
+          }
+          );
+      });
+})
+// NewPassword-------------------------------------------------------------------------------------------------------------------
+//reserve------------------------------------------------------------------------------------------------------------------
+app.post('/reserveroom', jsonParser, function (req, res) {
+  try {
+    // const token = req.headers.authorization.split(' ')[1]; // ดึง Token จากส่วนหัว Authorization
+    const decode = jwt.verify(req.body.token, secret);
+    const { username, user_id } = decode; // ดึง user_id จาก Token
+
+    if (username && user_id) { // ตรวจสอบว่า Token ถูกต้องและมี user_id
+      const startTime = req.body.start_time;
+      const endTime = req.body.end_time;
+      connection.execute(
+        'INSERT INTO reservation (start_time, end_time, user_id) VALUES (?, ?, ?)',
+        [startTime, endTime, user_id],
+        function (err, results, fields) {
+          if (err) {
+            res.json({ status: 'error', message: err });
+          } else {
+            res.json({ status: 'ok', message: 'Room reserved successfully' });
+          }
+        }
+      );
+    } else {
+      res.json({ status: 'error', message: 'Unauthorized' });
+    }
+  } catch (err) {
+    res.json({ status: 'error', message: err.message });
+  }
+});
 
 
 
+//reserve------------------------------------------------------------------------------------------------------------------
 
 app.listen(3333, function () {
     console.log('CORS-enabled web server listening on port 3333')
